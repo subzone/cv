0d750cc35a1700720576e2e54e44319c68add102
Milenko Mitrovic <subzone@users.noreply.github.com>
2024-08-04 06:30:43 +0200

    try10

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
 		<a href="https://www.linkedin.com/in/subzone/" target="_blank"><FaLinkedin /></a>&nbsp;&nbsp;&nbsp;&nbsp;
 		<a href="https://twitter.com/milenkom81" target="_blank"><FaTwitter /></a>&nbsp;&nbsp;&nbsp;&nbsp;
 		<a href="https://www.facebook.com/subzone" target="_blank"><FaFacebook /></a>
-		<hr /> 
+		<br /> 
 		<br /> {`Copyright © Milenko Mitrovic ${year}`}</footer>);
   };
   
