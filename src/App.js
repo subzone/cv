597faf676a4b771d767ac787253b3ce5f82a6edc
Milenko Mitrovic <subzone@users.noreply.github.com>
2024-08-04 10:48:20 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -12,16 +12,19 @@ import { Helmet } from 'react-helmet';
 import ContactForm from './components/ContactForm';
 import Chatbot from 'react-chatbot-kit';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 function App() {
   return (
     <BrowserRouter>
-      <nav>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/about" className="nav-link">About</Link>
         <Link to="/experience" className="nav-link">Experience</Link>
         <Link to="/education" className="nav-link">Education</Link>
         <Link to="/projects" className="nav-link">Projects</Link>
-        <Link to="/chatbot" className="nav-lin">Chatbot</Link>
+        {/* <Link to="/chatbot" className="nav-lin">Chatbot</Link> */}
       </nav>
       <div className='dark-theme'><p></p>
            
@@ -35,10 +38,12 @@ function App() {
         <Route path='/contact' element={<ContactForm />} />
         <Route path='/chatbot' element={<Chatbot />} />
       </Routes>
-      </div><div className='cv'> <Helmet>
-        <html lang="en" />
-        <meta name="theme-color" content="#E6E6FA" />
-      </Helmet><Footer /></div>
+      </div><div className='cv'> 
+        <Helmet>
+          <html lang="en" />
+          <meta name="theme-color" content="#E6E6FA" />
+      </Helmet>
+      <Footer /></div>
     </BrowserRouter>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,22 @@
+import React from "react";
 import { FaGithub, FaLinkedin, FaTwitter, FaFacebook } from "react-icons/fa/";
+import '../App.css';
 
 const Footer = () => {
 	const year = new Date().getFullYear();
-  
-	return (<footer>
-		<a href="https://github.com/subzone" target="_blank"><FaGithub /></a>&nbsp;&nbsp;&nbsp;&nbsp;
-		<a href="https://www.linkedin.com/in/subzone/" target="_blank"><FaLinkedin /></a>&nbsp;&nbsp;&nbsp;&nbsp;
-		<a href="https://twitter.com/milenkom81" target="_blank"><FaTwitter /></a>&nbsp;&nbsp;&nbsp;&nbsp;
-		<a href="https://www.facebook.com/subzone" target="_blank"><FaFacebook /></a>
-		<br /> 
-		<br /> {`Copyright © Milenko Mitrovic ${year}`}</footer>);
-  };
-  
-  export default Footer;
+
+	return (
+		<div className="footer custom-footer"> {/* Add the "custom-footer" class */}
+			<div className="container">			
+				<a href="https://github.com/subzone" target="_blank"><FaGithub /></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://www.linkedin.com/in/subzone/" target="_blank"><FaLinkedin /></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://twitter.com/milenkom81" target="_blank"><FaTwitter /></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="https://www.facebook.com/subzone" target="_blank"><FaFacebook /></a>
+				<br />
+				<p className="text-muted"> {`Copyright © Milenko Mitrovic ${year}.  All rights reserved.`}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
